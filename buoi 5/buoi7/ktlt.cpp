25f5a4b7faade76257cd21bd0844ff66d254ba44
Administrator <Administrator@PMT2002>
2026-04-13 14:54:44 +0700

    ktlt.123456

--- buoi 5/buoi7/ktlt.cpp
+++ buoi 5/buoi7/ktlt.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <string>
+#include < fstream>
 using namespace std;
+
 struct Author {
     int id;
     string name;
     friend istream& operator>>(istream& in, Author& a) {
-        cout << "Author information: " << endl;
+        cout << "Author information:" << endl;
         cout << "\t+ Id: ";
         in >> a.id;
         cout << "\t+ Name: ";
@@ -27,8 +29,8 @@ struct Book {
         return os;
     }
     friend istream& operator>>(istream& in, Book& b) {
-        cout << "Book information:" << endl;
-        cout << "\t= Id: ";
+        cout << "Book information: " << endl;
+        cout << "\t+ Id: ";
         in >> b.id;
         cout << "\t+ Name: ";
         in.ignore();
@@ -64,6 +66,82 @@ struct LinkedList {
         p->next = head;
         head = p;
     }
+    void Export(string filename) {
+        ofstream out(filename, ios::binary);
+        if (!out.is_open()) {
+            cout << " Cannot open file " << endl;
+            return;
+        }
+        while (head != NULL) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+
+        Node* item = head;
+        while (item != NULL) {
+            out.write(reinterpret_cast<const char*>(&item->data.id), sizeof(item->data.id));
+
+            size_t namelength = item->data.name.size();
+            out.write(reinterpret_cast<const char*>(&namelength), sizeof(namelength));
+            out.write(item->data.name.c_str(),namelength);
+
+
+            out.write(reinterpret_cast<const char*>(&item->data.author.id), sizeof(item->data.author.id));
+           
+            
+            size_t authornamelength = item->data.author.name.size();
+            out.write(reinterpret_cast<const char*> (&authornamelength), sizeof(authornamelength));
+            out.write(item->data.author.name.c_str(), authornamelength);
+              
+            item = item->next;
+
+        }
+        out.close();
+
+    }
+    void Import(string filename) {
+        ifstream in(filename, ios::binary);
+        if (!in.is_open()) {
+            cout << "CAnnot open file" << endl;
+            return;
+        }
+      
+    }
+
+    void Import(string filename) {
+        ifstream in(filename, ios::binary);
+        if (!in.is_open()) {
+            cout << "Cannot open file" << endl;
+            return;
+        }
+
+        while (in.peek() != EOF) {
+            Book b;
+
+            in.read(reinterpret_cast<char*>(&b.id), sizeof(b.id));
+
+            size_t namelength;
+            in.read(reinterpret_cast<char*>(&namelength), sizeof(namelength));
+            b.name.resize(namelength);
+            in.read(&b.name[0], namelength);
+
+            in.read(reinterpret_cast<char*>(&b.author.id), sizeof(b.author.id));
+
+            size_t authornamelength;
+            in.read(reinterpret_cast<char*>(&authornamelength), sizeof(authornamelength));
+            b.author.name.resize(authornamelength);
+            in.read(&b.author.name[0], authornamelength);
+
+            Node* newNode = new Node;
+            newNode->Create(b);
+            AddFirst(newNode);
+        }
+
+        in.close();
+    }
+
+
     bool Remove(int removeId) {
         if (head == NULL) {
             cout << "No book available" << endl;
@@ -75,8 +153,9 @@ struct LinkedList {
             delete item;
             return true;
         }
-        while (item->next != NULL) {
-            if (item->next->data.id == removeId) {
+
+        while (item != NULL) {
+            if (item->data.id == removeId) {
                 Node* temp = item->next;
                 item->next = item->next->next;
                 delete temp;
@@ -86,19 +165,35 @@ struct LinkedList {
         }
         return false;
     }
-    bool Update(int updateId) {
+    bool Updata(int updateId) {
         if (head == NULL) {
             cout << "No book available" << endl;
             return false;
         }
         Node* item = head;
         while (item != NULL) {
-            if (item->data.id == updateId)
+            if (item->data.id == updateId) {
                 cin >> item->data;
+                return true;
+            }
+            item = item->next;
         }
-        item = item->next;
+        return false;
     }
-    return false;
+    Book* Find(string bookName) {
+        if (head == NULL) {
+            cout << "No book available" << endl;
+            return NULL;
+        }
+        Node* item = head;
+        while (item != NULL) {
+            if (item->data.name.find(bookName) != std::string::npos) {
+                return &(item->data);
+            }
+            item = item->next;
+        }
+        return NULL;
+    }
 };
 
 int main()
@@ -106,16 +201,16 @@ int main()
     LinkedList books = { NULL };
     do {
         system("cls");
-        cout << "------- BOOK MANAGEMENT-------" << endl;
-        cout << "1. Show all book" << endl;
+        cout << "-------- BOOK MANAGEMENT --------" << endl;
+        cout << "1. Show all books" << endl;
         cout << "2. Add a book" << endl;
         cout << "3. Delete a book" << endl;
         cout << "4. Update a book" << endl;
         cout << "5. Find book" << endl;
         cout << "6. Export to file" << endl;
-        cout << "7. Import from file" << endl;
+        cout << "7. Import to file" << endl;
         cout << "0. Exit" << endl;
-        cout << "---------------------------------------" << endl;
+        cout << "---------------------------------" << endl;
         cout << "Enter your choice: ";
         int choice;
         cin >> choice;
@@ -148,7 +243,7 @@ int main()
             int updateId;
             cout << "Enter book's id to update: ";
             cin >> updateId;
-            bool res = books.Update(updateId);
+            bool res = books.Updata(updateId);
             if (res)
                 cout << "Update book successfully" << endl;
             else
@@ -156,9 +251,23 @@ int main()
             break;
         }
         case 5: {
+            string bookName;
+            cout << "Enter book's name to find: ";
+            cin.ignore();
+            getline(cin, bookName);
+            Book* res = books.Find(bookName);
+            if (res != NULL) {
+                cout << res;
+            }
+            else {
+                cout << "No book with name: " << bookName << endl;
+            }
             break;
         }
         case 6: {
+            books.Export("25TH1.dla");
+            cout << " Export successfilly " << endl;
+
             break;
         }
         case 7: {
@@ -167,12 +276,11 @@ int main()
         case 0: {
             return 0;
         }
-        default: {
+        default:
             cout << "Invalid choice, try again" << endl;
             break;
         }
-        }
-        system(" pause");
+        system("pause");
         cout << "Press any key to continue...";
     } while (true);
-}
+};
